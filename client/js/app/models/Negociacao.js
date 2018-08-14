594a19e243179ba56cccee2194b0b5eae94554be
felipeeps <felipeepereiras@gmail.com>
2018-08-13 22:52:39 -0300

    Encapsulando os dados da Negociacao

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,14 +1,28 @@
 //Criando minha classe
+//Convenção - atributos que não devem ser acessados pela classe contem o prefixo _
 class Negociacao { 
     //Definindo meu metodo construtor
     constructor(data, quantidade,valor) {
         //Associando a data com o Date
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     obtemVolume(){
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
+    }
+
+    //Encapsulando os campos
+    getData() {
+        return this._data;
+    }
+    
+    getQuantidade() {
+        return this._quantidade;
+    }
+    
+    getValor() {
+        return this._valor;
     }
 }
