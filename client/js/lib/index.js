ca0eec3ec5aad97d027733bf13964fd4200c18ce
felipeeps <felipeepereiras@gmail.com>
2018-08-13 21:52:53 -0300

    Listando os dados do form

--- client/js/lib/index.js
+++ client/js/lib/index.js
@@ -7,16 +7,31 @@ var campos = [
 
 console.log(campos);
 
+//Busca o meu TBODY do html
+var tbody = document.querySelector('table tbody');
+
 //Selecionando meu formulario e colocando uma função no meu botão de envio
 document.querySelector('.form').addEventListener('submit', function(event) {
 
-    //Cirando uma TR
+    //Não submete o formulário (Não recarrega a página)
+    event.preventDefault();
+
+    //Criando uma TR
     var tr = document.createElement('tr');
-    //Carrendo meu array e retornando os elementos inseridos no form
+    //Carregando meu array e retornando os elementos inseridos no form
     campos.forEach(function(campo){
         var td = document.createElement('td');
         td.textContent = campo.value;
         //Adiona um filho
         tr.appendChild(td);
-    })
+    });
+
+    //Criando a operação para calcular o volume
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    //Inserindo a minha tbody os valores do form
+    tbody.appendChild(tr);
+
 });
