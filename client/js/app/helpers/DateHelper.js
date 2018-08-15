293ce329ffb083a024a67b7c1d7a0213f6402d7f
felipeeps <felipeepereiras@gmail.com>
2018-08-14 22:33:12 -0300

    Criando erro para data invalida

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,22 +1,25 @@
 class DateHelper {
 
+    //Static - Invoco sem a necessidade de uma instancia
     constructor(){ 
         throw new Error('DateHelper não pode ser instanciado'); //PQ estou usando o static
     }
 
     //Colocando a data no formato PT/BR
     static dataParaTexto(data){
-        return data.getDate()
-        //Adicionando + 1 antes de concatenar 
-         + '/' + (data.getMonth() + 1)
-         + '/' + data.getFullYear();
+        //Concatenando com template String
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
  
     //Split - tranforma a string em um array / ... Indica os 3 parametros do meu array / MAP = Cria um array igual
     static textoParaData(texto){
+
+        //Criando minha expressão regular para validar o argumento (data) passado e testo se está no padrão
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
         return new Date(...texto.split('-')
         //1 modulo de 2 é 1, evitando o IF / Usando uma Arrow Function que evita o uso de "function" e return
         .map((item, indice) => item - indice % 2));
     }
-    //Static - Invoco sem a necessidade de uma instancia
 }
